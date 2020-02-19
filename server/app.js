7a8661da4e8f67abb30f4da84479406e430a247c
Asten Fuller <astenhfuller@gmail.com>
2020-02-19 13:29:44 -0800

    changed res.send

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.status(200).send('ok');
+    res.status(200).send('go to /logs for request info');
 });
 
 app.get('/logs', (req, res) => {
